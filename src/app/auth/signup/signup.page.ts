a44ce0d7d38b7e7cc5a401c23f191a98004f0f8c
l00pinfinity <collinskipkemboi001@gmail.com>
2022-10-20 13:18:33 +0300

    implement sign up form

--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,6 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { PasswordValidators } from 'src/app/core/validators/password-validators';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,52 +12,74 @@ import { PasswordValidators } from 'src/app/core/validators/password-validators'
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  
-  signupForm:FormGroup;
 
-  constructor() { 
+  signupForm: FormGroup;
+  signupStatus: any;
+  errorMessage:string;
+
+  constructor(private router: Router, private authService: AuthService, private toastCtrl: ToastController) {
     this.signupForm = this.createFormGroup();
   }
 
   createFormGroup(): FormGroup<any> {
     return new FormGroup({
-      email:new FormControl('',[Validators.required,Validators.email]),
-      username:new FormControl('',Validators.required),
-      password:new FormControl('',[Validators.required,Validators.minLength(6)]),
-      confirmpassword:new FormControl('',[Validators.required,Validators.minLength(6)]),
-      bio:new FormControl('',Validators.required)
-    },PasswordValidators.MatchValidator('password','confirmpassword'))
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmpassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      bio: new FormControl('', Validators.required)
+    }, PasswordValidators.MatchValidator('password', 'confirmpassword'))
   }
 
   ngOnInit() {
   }
 
-  onSignUp(){
-    console.log(this.signupForm.value)
+  onSignUp() {
+    if (this.email && this.username, this.password, this.bio) {
+      this.authService.signup(this.email.value, this.username.value, this.password.value, this.bio.value).subscribe((response: any) => {
+        if (response.success == true) {
+          this.router.navigateByUrl('/login');
+        }else{
+          this.signupStatus = response;
+          this.errorMessage = this.signupStatus.message;
+        }
+      }, async (error: Error | HttpErrorResponse) => {
+        const toast = this.toastCtrl.create({
+          message: `${error.message}`,
+          duration: 10000,
+          position: 'bottom',
+          color: 'danger'
+        });
+        (await toast).present();
+        setTimeout(async () => {
+          (await toast).dismiss();
+        }, 3000)
+      })
+    }
   }
 
   //getters
-  get email(){
+  get email() {
     return this.signupForm.get('email');
   }
 
-  get username(){
+  get username() {
     return this.signupForm.get('username');
   }
 
-  get password(){
+  get password() {
     return this.signupForm.get('password');
   }
 
-  get confirmpassword(){
+  get confirmpassword() {
     return this.signupForm.get('confirmpassword');
   }
 
-  get bio(){
+  get bio() {
     return this.signupForm.get('bio');
   }
 
-  get passwordMatchError(){
+  get passwordMatchError() {
     return (this.signupForm.getError('mismatch') && this.signupForm.get('confirmpassword')?.touched)
   }
 
