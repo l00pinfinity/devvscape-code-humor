30016701b9baade64bf432af456bdd6f7a5e8e83
l00pinfinity <collinskipkemboi001@gmail.com>
2022-11-12 15:35:23 +0300

    feat: create whitespace validator to avoid extra spaces on account creation

---
+++ src/app/core/validators/whitespace-validators.spec.ts
@@ -0,0 +1,7 @@
+import { WhitespaceValidators } from './whitespace-validators';
+
+describe('WhitespaceValidators', () => {
+  it('should create an instance', () => {
+    expect(new WhitespaceValidators()).toBeTruthy();
+  });
+});
