74dfdf528e485cfcafc8b2f3cae05304e6630c1c
l00pinfinity <collinskipkemboi001@gmail.com>
2023-02-20 13:55:15 +0300

    remove view image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guard/auth.guard';
-import { DataResolverService } from './core/services/data-resolver.service';
 
 const routes: Routes = [
   {
@@ -20,14 +19,6 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./components/home/home.module').then( m => m.HomePageModule),
-    canActivate:[AuthGuard]
-  },
-  {
-    path: 'images/:id',
-    resolve: {
-      special: DataResolverService
-    },
-    loadChildren: () => import('./components/view-images/view-images.module').then( m => m.ViewImagesPageModule),
     canActivate:[AuthGuard]
   },
   {
